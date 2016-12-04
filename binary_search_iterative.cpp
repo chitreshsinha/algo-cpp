abf1fc29f7591c770107efc4f6035a4065eeaad2
chitreshbb <chitreshsinha@blackbuck.com>
2016-12-04 19:31:43 +0530

    binary search iterative

--- binary_search_iterative.cpp
+++ binary_search_iterative.cpp
@@ -9,14 +9,14 @@ int binarySearch(const vector<int> &V, int num) {
 	int min = 0;
 	int mid;
 
-	while(min!=max) {
+	while(min<=max) {
 		mid = (min+max)/2;
 		if(V[mid] == num) {
 			return mid;
 		} else if (num > V[mid]) {
 			min = mid+1;
 		} else if (num < V[mid]) {
-			max = mid;
+			max = mid-1;
 		}
 	}
 	return -1;
