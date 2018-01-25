6d38ac0316a1b2b8e42e02090831df36dd627756
chitreshbb <chitreshsinha@blackbuck.com>
2018-01-25 23:10:59 +0530

    string subsequence

--- string_subsequence.cpp
+++ string_subsequence.cpp
@@ -7,19 +7,25 @@ map<string, bool> m;
 
 void printSub(string s) {
     //cout << s << endl;
-    m[s] = true;
-    int len = s.size();
-    
-    for(int i=0;i<len;i++) {
-        string ns = s.substr(0, i) + s.substr(i+1, len);
-        //cout << ns << endl;
-        printSub(ns);
-        //cout << s.substr(0, i) << " " << s.substr(i+1, len) << endl;
+    if(s.size()==0) {
+    	return;
+    }
+
+    if(m.find(s) == m.end()) {
+    	m[s] = true;
+	    int len = s.size();
+	    
+	    for(int i=0;i<len;i++) {
+	        string ns = s.substr(0, i) + s.substr(i+1, len);
+	        //cout << ns << endl;
+	        printSub(ns);
+	        //cout << s.substr(0, i) << " " << s.substr(i+1, len) << endl;
+	    }
     }
 }
 
 int main() {
-    string s = "ABCD";
+    string s = "xyzx";
     printSub(s);
     map<string, bool>::iterator it;
     for(it=m.begin();it!=m.end();it++) {
